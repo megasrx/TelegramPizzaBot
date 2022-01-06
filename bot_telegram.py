13ccb1482903aa1c14e02b3c29eb86312eb4d417
megasrx <megasrx@gmail.com>
2022-01-07 01:12:33 +0500

    adding bad words filter

--- bot_telegram.py
+++ bot_telegram.py
@@ -1,9 +1,11 @@
+import json
+import string
+
 from aiogram import Bot, types
 from aiogram.dispatcher import Dispatcher
 from aiogram.utils import executor
 
 from config import telegram_bot_token, telegram_bot_username
-
 
 bot = Bot(token=telegram_bot_token)
 dp = Dispatcher(bot)
@@ -19,7 +21,8 @@ async def command_start(message: types.Message):
         await bot.send_message(message.from_user.id, "Salom! Yoqimli ishataha")
         await message.delete()
     except:
-        await message.reply(f"Bot bilan chatda suhbatlashish uchun unga start buyrug'ini bering. {telegram_bot_username}")
+        await message.reply(
+            f"Bot bilan chatda suhbatlashish uchun unga start buyrug'ini bering. {telegram_bot_username}")
 
 
 @dp.message_handler(commands=['Ish_tartibi'])
@@ -30,6 +33,14 @@ async def pizza_open_command(message: types.Message):
 @dp.message_handler(commands=['Manzil'])
 async def pizza_place_command(message: types.Message):
     await bot.send_message(message.from_user.id, "Toshkent shahri")
+
+
+@dp.message_handler()
+async def bad_word_filter(message: types.Message):
+    if {i.lower().translate(str.maketrans('', '', string.punctuation)) for i in message.text.split(" ")} \
+            .intersection(set(json.load(open("bad_words.json")))) != set():
+        await message.reply("Guruhda haqoratli so\'zlar ishlatish ta\'qiqlangan")
+        await message.delete()
 
 
 executor.start_polling(dp, skip_updates=True, on_startup=on_startup)
