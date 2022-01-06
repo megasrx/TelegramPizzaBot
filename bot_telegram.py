14353bf2210f03a7b86580478df0d5679d380e2b
megasrx <megasrx@gmail.com>
2022-01-07 00:47:04 +0500

    adding basic commands handlers start,help, ishtartibi, manzil

--- bot_telegram.py
+++ bot_telegram.py
@@ -2,12 +2,34 @@ from aiogram import Bot, types
 from aiogram.dispatcher import Dispatcher
 from aiogram.utils import executor
 
-from config import telegram_bot_token
+from config import telegram_bot_token, telegram_bot_username
 
 
 bot = Bot(token=telegram_bot_token)
 dp = Dispatcher(bot)
 
 
+async def on_startup(_):
+    print("Bot online rejimda ishlamoqda.")
 
-executor.start_polling(dp, skip_updates=True)
+
+@dp.message_handler(commands=['start', 'help'])
+async def command_start(message: types.Message):
+    try:
+        await bot.send_message(message.from_user.id, "Salom! Yoqimli ishataha")
+        await message.delete()
+    except:
+        await message.reply(f"Bot bilan chatda suhbatlashish uchun unga start buyrug'ini bering. {telegram_bot_username}")
+
+
+@dp.message_handler(commands=['Ish_tartibi'])
+async def pizza_open_command(message: types.Message):
+    await bot.send_message(message.from_user.id, "Har kuni, 24 soat")
+
+
+@dp.message_handler(commands=['Manzil'])
+async def pizza_place_command(message: types.Message):
+    await bot.send_message(message.from_user.id, "Toshkent shahri")
+
+
+executor.start_polling(dp, skip_updates=True, on_startup=on_startup)
--- config.py
+++ config.py
@@ -4,3 +4,4 @@ from dotenv import load_dotenv
 load_dotenv()
 
 telegram_bot_token = os.getenv("BOT_TOKEN")
+telegram_bot_username=os.getenv("BOT_USERNAME")
