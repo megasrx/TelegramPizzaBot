9f614f1c13f172a31defa3488520592dd4758eae
megasrx <megasrx@gmail.com>
2022-01-09 00:11:30 +0500

    added checking is admin, cancel state

--- handlers/admin.py
+++ handlers/admin.py
@@ -1,7 +1,10 @@
 from aiogram.dispatcher import FSMContext
+from aiogram.dispatcher.filters import Text
 from aiogram.dispatcher.filters.state import State, StatesGroup
 from aiogram import types, Dispatcher
-from create_bot import dp
+from create_bot import dp, bot
+
+ID = None
 
 
 class FSMAdmin(StatesGroup):
@@ -11,37 +14,58 @@ class FSMAdmin(StatesGroup):
     price = State()
 
 
+async def make_changes_command(message: types.Message):
+    global ID
+    ID = message.from_user.id
+    await bot.send_message(ID, "Yangi mahsulot qo\'shamizmi?")
+    await message.delete()
+
+
 async def cm_start(message: types.Message):
-    await FSMAdmin.photo.set()
-    await message.reply("Rasm yuboring")
+    if message.from_user.id == ID:
+        await FSMAdmin.photo.set()
+        await message.reply("Rasm yuboring")
 
 
 async def load_photo(message: types.Message, state: FSMContext):
-    async with state.proxy() as data:
-        data['photo'] = message.photo[0].file_id
-    await FSMAdmin.next()
-    await message.reply("Endi nomini kiriting")
+    if message.from_user.id == ID:
+        async with state.proxy() as data:
+            data['photo'] = message.photo[0].file_id
+        await FSMAdmin.next()
+        await message.reply("Endi nomini kiriting")
 
 
 async def load_name(message: types.Message, state: FSMContext):
-    async with state.proxy() as data:
-        data['name'] = message.text
-    await FSMAdmin.next()
-    await message.reply("Mahsulot ta\'rifini kiriting")
+    if message.from_user.id == ID:
+        async with state.proxy() as data:
+            data['name'] = message.text
+        await FSMAdmin.next()
+        await message.reply("Mahsulot ta\'rifini kiriting")
 
 
 async def load_description(message: types.Message, state: FSMContext):
-    async with state.proxy() as data:
-        data['description'] = message.text
-    await FSMAdmin.next()
-    await message.reply("Narxini kiriting")
+    if message.from_user.id == ID:
+        async with state.proxy() as data:
+            data['description'] = message.text
+        await FSMAdmin.next()
+        await message.reply("Narxini kiriting")
 
 
 async def load_price(message: types.Message, state: FSMContext):
-    async with state.proxy() as data:
-        data['price'] = float(message.text)
+    if message.from_user.id == ID:
+        async with state.proxy() as data:
+            data['price'] = float(message.text)
 
-    await state.finish()
+        await state.finish()
+
+
+async def cancel_handler(message: types.Message, state: FSMContext):
+    if message.from_user.id == ID:
+        current_state = await state.get_state()
+        if current_state is None:
+            return
+        await state.finish()
+        await message.reply('Yuklash to\'xtatildi.')
 
 
 def register_handlers_client(dispatcher: Dispatcher):
@@ -50,3 +74,6 @@ def register_handlers_client(dispatcher: Dispatcher):
     dispatcher.register_message_handler(load_name, state=FSMAdmin.name)
     dispatcher.register_message_handler(load_description, state=FSMAdmin.description)
     dispatcher.register_message_handler(load_price, state=FSMAdmin.price)
+    dispatcher.register_message_handler(cancel_handler, state="*", commands=['Bekor_qilish'])
+    dispatcher.register_message_handler(cancel_handler, Text(equals='Bekor_qilish', ignore_case=True), state='*')
+    dispatcher.register_message_handler(make_changes_command, commands=['moderator'], is_chat_admin=True)
